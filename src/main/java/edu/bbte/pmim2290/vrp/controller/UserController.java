9801b715cea87447076973b38e3e47df72db7c4a
Portik Márk-Krisztián <mark.portik@stud.ubbcluj.ro>
2025-08-22 18:08:08 +0300

    Users works with Authn/Authz

--- src/main/java/edu/bbte/pmim2290/vrp/controller/UserController.java
+++ src/main/java/edu/bbte/pmim2290/vrp/controller/UserController.java
@@ -1,5 +1,6 @@
 package edu.bbte.pmim2290.vrp.controller;
 
+import edu.bbte.pmim2290.vrp.config.UserDetailsImpl;
 import edu.bbte.pmim2290.vrp.dto.InUserDTO;
 import edu.bbte.pmim2290.vrp.dto.OutUserDTO;
 import edu.bbte.pmim2290.vrp.exception.DatabaseException;
@@ -8,19 +9,21 @@ import edu.bbte.pmim2290.vrp.mapper.UserMapper;
 import edu.bbte.pmim2290.vrp.model.User;
 import edu.bbte.pmim2290.vrp.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private final UserService userService;
 
@@ -31,50 +34,33 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public List<OutUserDTO> getUsers()
-            throws DatabaseException {
-        List<User> users;
-        users = userService.getAllUsers();
-
-
-        return users.stream()
-                .map(userMapper::toOutUserDTO)
-                .collect(Collectors.toList());
+    private User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+        return userDetails.getUser();
     }
 
     @GetMapping("/{id}")
     public OutUserDTO getUser(@PathVariable Long id) throws EntityNotFoundException, DatabaseException {
+        User authUser = getCurrentUser();
+        if (!authUser.getId().equals(id)) {
+            throw new SecurityException("Access denied");
+        }
         Optional<User> user = userService.getUserById(id);
         return userMapper.toOutUserDTO(user.get());
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<OutUserDTO> createUser(@Valid @RequestBody InUserDTO inUser)
-            throws DatabaseException, EntityNotFoundException {
-        User user = userMapper.toUser(inUser);
-        URI uri = URI.create("api/Users/" + user.getId());
-        return ResponseEntity.created(uri).body(userMapper.toOutUserDTO(userService.createUser(user)));
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<OutUserDTO> updateUser(@PathVariable Long id, @Valid @RequestBody InUserDTO inUser)
             throws EntityNotFoundException, DatabaseException {
+        User authUser = getCurrentUser();
+
+        if (!authUser.getId().equals(id)) {
+            throw new SecurityException("Access denied");
+        }
         User user = userMapper.toUser(inUser);
         user.setId(id);
         return ResponseEntity.ok(userMapper.toOutUserDTO(userService.updateUser(user)));
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable Long id) throws EntityNotFoundException, DatabaseException {
-        Optional<User> user = userService.getUserById(id);
-        if (user.isEmpty()) {
-            throw new EntityNotFoundException("The requested User does not exist");
-        }
-
-        userService.deleteUser(id);
     }
 }
